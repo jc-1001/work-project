cc95cd55b30d8d7c561af52dfa63d37a602fb377
CERE\angel0970 <macio6898@gmail.com>
2026-04-08 15:31:33 +0800

    權限攔截

--- vue/src/views/front/shop/ProductDetail.vue
+++ vue/src/views/front/shop/ProductDetail.vue
@@ -1,11 +1,20 @@
 <script setup>
 import { ref, onMounted } from "vue";
-import { ElRow, ElCol, ElCard, ElImage, ElButton, ElMessage } from "element-plus";
-import { useRoute } from "vue-router";
+import {
+  ElRow,
+  ElCol,
+  ElCard,
+  ElImage,
+  ElButton,
+  ElMessage,
+  ElMessageBox,
+} from "element-plus";
+import { useRoute, useRouter } from "vue-router";
 import api from "../../../api";
 
 const product = ref({});
 const route = useRoute();
+const router = useRouter();
 const num = ref(1);
 // 抓取商品資料
 const fetchProductDetail = async () => {
@@ -19,39 +28,58 @@ const fetchProductDetail = async () => {
 };
 // 商品數量監聽
 const handleChange = (value) => {
-//   console.log("當前商品選擇的數量:", value); // 測試
-  if(value > product.value.stock){
+  //   console.log("當前商品選擇的數量:", value); // 測試
+  if (value > product.value.stock) {
     ElMessage.warning("庫存不足~ 無法再增加數量");
     num.value = product.value.stock; // 將數量限制在庫存範圍內
   }
 };
 
-// 加入購物車邏輯
+// 加入購物車邏輯(非會員不可以點進去)
 const addToCart = () => {
-    // 讀取localstorage中是否有購物車資料，沒有就給空陣列
-    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  // 檢查是否有登入，沒有就提示登入
+  const token = localStorage.getItem("token");
 
-// 檢查是否已經買過這個商品了，如果有就更新數量，沒有就新增一筆
-const index = cart.findIndex(item => item.id === product.value.id);
+  if (!token) {
+    ElMessageBox.confirm("您尚未登入，是否要前往登入頁面？", "提示", {
+      confirmButtonText: "前往登入",
+      cancelButtonText: "取消",
+      type: "warning",
+    })
+      .then(() => {
+        // 使用者點了前往登入
+        router.push("/login");
+      })
+      .catch(() => {
+        // 使用者點了取消，留在原地
+      });
+    return;
+  }
+  // 登入之後會執行下面
+  // 讀取localstorage中是否有購物車資料，沒有就給空陣列
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-// 如果買過就增加數量
-if (index !== -1) {
-  cart[index].quantity += num.value;
-} else {
-  // 沒有就把新商品推進去
-  cart.push({ 
-    id: product.value.id,
-    name: product.value.name,
-    price: product.value.price,
-    image: product.value.image,
-    quantity: num.value
-   });
-}
+  // 檢查是否已經買過這個商品了，如果有就更新數量，沒有就新增一筆
+  const index = cart.findIndex((item) => item.id === product.value.id);
 
-// 將更新後的購物車資料儲存到 localStorage
-localStorage.setItem("cart", JSON.stringify(cart));
-ElMessage.success("已加入購物車!!!");
-}
+  // 如果買過就增加數量
+  if (index !== -1) {
+    cart[index].quantity += num.value;
+  } else {
+    // 沒有就把新商品推進去
+    cart.push({
+      id: product.value.id,
+      name: product.value.name,
+      price: product.value.price,
+      image: product.value.image,
+      quantity: num.value,
+    });
+  }
+
+  // 將更新後的購物車資料儲存到 localStorage
+  localStorage.setItem("cart", JSON.stringify(cart));
+  ElMessage.success("已加入購物車!!!");
+};
 
 onMounted(() => {
   fetchProductDetail();
@@ -75,37 +103,40 @@ onMounted(() => {
         </template>
       </el-col>
 
-        <el-col :md="12" class="good-des">
-          <h1>{{ product.name }}</h1>
-          <h2 style="color: #f56c6c">NT$ {{ product.price }}</h2>
-          <p>商品描述：{{ product.description }}</p>
-          <p>庫存數量：{{ product.stock }}</p>
+      <el-col :md="12" class="good-des">
+        <h1>{{ product.name }}</h1>
+        <h2 style="color: #f56c6c">NT$ {{ product.price }}</h2>
+        <p>商品描述：{{ product.description }}</p>
+        <p>庫存數量：{{ product.stock }}</p>
 
-          <div class="good-num">
-            <el-input-number v-model="num" :min="1" :max="10" @change="handleChange" />
-            <el-button type="primary" size="large" @click="addToCart">
-              加入購物車
-            </el-button>
-          </div>
-
-        </el-col>
-      
+        <div class="good-num">
+          <el-input-number
+            v-model="num"
+            :min="1"
+            :max="10"
+            @change="handleChange"
+          />
+          <el-button type="primary" size="large" @click="addToCart">
+            加入購物車
+          </el-button>
+        </div>
+      </el-col>
     </el-row>
   </div>
 </template>
-<style >
-  .back-shop {
-    margin: 20px 0;
-  }
-  .good-des {
-    border: 1px solid #eee;
-    border-radius: 20px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  }
-  .good-num{
-    display: flex;
-    gap: 10px;
-  }
+<style>
+.back-shop {
+  margin: 20px 0;
+}
+.good-des {
+  border: 1px solid #eee;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.good-num {
+  display: flex;
+  gap: 10px;
+}
 </style>
